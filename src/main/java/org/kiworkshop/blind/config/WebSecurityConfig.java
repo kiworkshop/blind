e9ced133ebff902f098dc0ba15fa738faa61ca17
myang <kmh4503@gmail.com>
2020-08-08 09:22:29 +0900

    Add roles and change config

--- src/main/java/org/kiworkshop/blind/config/WebSecurityConfig.java
+++ src/main/java/org/kiworkshop/blind/config/WebSecurityConfig.java
@@ -1,31 +1,42 @@
 package org.kiworkshop.blind.config;
 
+import org.kiworkshop.blind.user.domain.Role;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
+    public void configure(WebSecurity web) {
+        web.ignoring().antMatchers("/css/**", "/js/**", "/img/**");
+    }
+
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
             .csrf().disable()
             .authorizeRequests()
-                .antMatchers("/","/users").permitAll()
-                .anyRequest().authenticated()
+                .antMatchers("/**","/users").permitAll()
+                .antMatchers("/admin/**").hasRole("ADMIN")
+                .antMatchers("/member/**").authenticated()
+                .antMatchers("/admin/**").authenticated()
                 .and()
             .formLogin()
                 .loginPage("/login")
+                .defaultSuccessUrl("/home")
                 .permitAll()
                 .and()
             .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
                 .logoutSuccessUrl("/login")
-                .invalidateHttpSession(true)
-                .permitAll();
+                .invalidateHttpSession(true);
     }
 
     @Bean
---
+++ src/main/java/org/kiworkshop/blind/user/domain/Role.java
@@ -0,0 +1,13 @@
+package org.kiworkshop.blind.user.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum Role {
+    ADMIN("ROLE_ADMIN"),
+    USER("ROLE_USER");
+
+    private String value;
+}
