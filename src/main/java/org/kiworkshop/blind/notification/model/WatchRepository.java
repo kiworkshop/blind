37a05158d4788bd83d1c437efe28e43eb0c33d1e
jaeju.jang <jjj0611@gmail.com>
2020-08-15 11:17:18 +0900

    feat: repository query 추가

--- src/main/java/org/kiworkshop/blind/notification/model/WatchRepository.java
+++ src/main/java/org/kiworkshop/blind/notification/model/WatchRepository.java
@@ -7,6 +7,7 @@ import org.kiworkshop.blind.post.domain.Post;
 import org.kiworkshop.blind.user.domain.User;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface WatchRepository extends JpaRepository<Watch, Long> {
     boolean existsByPostAndUser(Post post, User user);
@@ -14,8 +15,10 @@ public interface WatchRepository extends JpaRepository<Watch, Long> {
     Optional<Watch> findByPostAndUser(Post post, User user);
 
     @EntityGraph(attributePaths = {"post", "post.author", "post.likes"})
+    @Query("SELECT w FROM Watch w WHERE w.user = :user")
     List<Watch> findAllByUser(User user);
 
     @EntityGraph(attributePaths = {"user"})
+    @Query("SELECT w FROM Watch w WHERE w.post = :post")
     List<Watch> findAllByPost(Post post);
 }
