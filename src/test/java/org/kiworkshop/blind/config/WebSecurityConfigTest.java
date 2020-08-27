681f2aa412201c801522eb1c157b17132f7b4038
myang <kmh4503@gmail.com>
2020-08-27 19:43:26 +0900

    Update Security test

--- src/test/java/org/kiworkshop/blind/config/WebSecurityConfigTest.java
+++ src/test/java/org/kiworkshop/blind/config/WebSecurityConfigTest.java
@@ -1,22 +1,16 @@
 package org.kiworkshop.blind.config;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.*;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.logout;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.*;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*;
-import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
-import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.unauthenticated;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.*;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import java.util.Optional;
 
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpSession;
-
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -27,19 +21,13 @@ import org.kiworkshop.blind.user.repository.UserRepository;
 import org.kiworkshop.blind.user.service.UserService;
 import org.kiworkshop.blind.user.util.PasswordEncryptor;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
-import org.springframework.mock.web.MockHttpServletResponse;
-import org.springframework.mock.web.MockHttpSession;
-import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
-import org.springframework.security.web.FilterChainProxy;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
@@ -52,7 +40,7 @@ class WebSecurityConfigTest {
     private WebApplicationContext context;
 
     @Mock
-    private UserRepository repository;
+    private UserRepository userRepository;
 
     @Autowired
     UserService userService;
@@ -66,17 +54,19 @@ class WebSecurityConfigTest {
             .build();
     }
 
+    @AfterEach
+    void tearDown() {
+        userRepository.deleteAll();
+    }
+
     @Test
-    @DisplayName("login이 되면 200 response를 받는다")
+    @DisplayName("login이 되면 302 response를 받는다")
     public void login_test() throws Exception {
-        when(repository.findByEmail("harris")).thenReturn(Optional.of(getUserFixture()));
-        MvcResult result = mvc.perform(post("/login")
+        when(userRepository.findByEmail("harris")).thenReturn(Optional.of(getUserFixture()));
+        mvc.perform(post("/login")
             .with(user("harris").password("1234"))
             .contentType("application/json"))
-            .andExpect(status().is(HttpStatus.MOVED_TEMPORARILY.value())).andReturn();
-        Cookie cookie = result.getResponse().getCookie("JSESSIONID");
-        System.out.println(cookie);
-        then(repository).should().findByEmail(any(String.class));
+            .andExpect(status().is(HttpStatus.MOVED_TEMPORARILY.value()));
     }
 
     private User getUserFixture() {
@@ -90,10 +80,10 @@ class WebSecurityConfigTest {
     @DisplayName("formLogin으로 로그인 성공 테스트")
     void formLoginSuccessTest() throws Exception {
         userService.createUser(UserRequestDto.builder().email("harris").password("1234").build());
-        MvcResult result = mvc.perform(formLogin().user("harris").password("1234"))
-                .andExpect(authenticated())
-                .andReturn();
+        mvc.perform(formLogin().user("harris").password("1234"))
+                .andExpect(authenticated());
     }
+
     @Test
     @DisplayName("formLogin으로 로그인 실패 테스트")
     void formLoginFailTest() throws Exception {
@@ -106,9 +96,8 @@ class WebSecurityConfigTest {
     @DisplayName("logout으로 로그아웃 성공 테스트")
     void formLogoutTest() throws Exception {
         userService.createUser(UserRequestDto.builder().email("harris").password("1234").build());
-        MvcResult result = mvc.perform(formLogin().user("harris").password("1234"))
-                .andExpect(authenticated())
-                .andReturn();
+        mvc.perform(formLogin().user("harris").password("1234"))
+                .andExpect(authenticated());
         mvc.perform(logout("/logout")).andExpect(unauthenticated());
     }
 }
